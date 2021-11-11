caebd47c886107bdaa33e2a5800833f0d9e8447e
alexanderbonnet <alex.bonnet.us@gmail.com>
2021-11-11 20:20:58 +0100

    add sequential python binding & clean makefile

--- Makefile
+++ Makefile
@@ -3,22 +3,16 @@ CFLAGS = -std=c99 -Wall -fopenmp -O3
 UTILS = src/utils.c src/copy.c src/linalg.c
 
 compile-seq:
-	${CC} ${CFLAGS} src/be_hpc_seq.c ${UTILS} -o src/bin/be_hpc_seq
+	${CC} ${CFLAGS} src/be_hpc_seq.c ${UTILS} -o src/bin/be_hpc_seq.o
 
 run-seq:
-	./src/bin/be_hpc_seq
-
-compile-run-seq:
-	${CC} ${CFLAGS} src/be_hpc_seq.c ${UTILS} -o src/bin/be_hpc_seq && ./src/bin/be_hpc_seq
+	./src/bin/be_hpc_seq.o
 
 compile-par:
-	${CC} ${CFLAGS} src/be_hpc_parallel.c ${UTILS} -o src/bin/be_hpc_parallel
+	${CC} ${CFLAGS} src/be_hpc_parallel.c ${UTILS} -o src/bin/be_hpc_parallel.o
 
 run-par:
-	./src/bin/be_hpc_parallel
-
-compile-run-par:
-	${CC} ${CFLAGS} src/be_hpc_parallel.c ${UTILS} -o src/bin/be_hpc_parallel && ./src/bin/be_hpc_parallel
+	./src/bin/be_hpc_parallel.o
 
 clean-bin:
 	rm -rf ./src/bin/*
--- python_binding/jacobi_method.py
+++ python_binding/jacobi_method.py
@@ -15,9 +15,6 @@ def main():
 
     ARGS = parser.parse_args()
 
-    if ARGS.num_threads < 1:
-        raise ValueError("num_threads must be striclty larger than 1!")
-
     out_path = Path("./src/bin")
     if not out_path.exists():
         out_path.mkdir(parents=True)
@@ -36,8 +33,15 @@ def main():
             f"{ARGS.num_threads} "
         )
         subprocess.run(run_cmd, shell=True)
+    elif ARGS.num_threads == 1:
+        compile_cmd = f"{CC} {CFLAGS} src/be_hpc_seq.c {UTILS} -o src/bin/be_hpc_seq.o"
+        subprocess.run(compile_cmd, shell=True)
+
+        run_cmd = f"./src/bin/be_hpc_seq.o " f"{ARGS.problem_dimension} "
+        subprocess.run(run_cmd, shell=True)
     else:
-        pass
+        # explicitly handle error
+        raise ValueError("num_threads must be striclty larger than 1!")
 
 
 if __name__ == "__main__":
