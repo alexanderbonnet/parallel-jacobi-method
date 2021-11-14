2742e927a8c9249c97be2eaf86100ef390c89c1b
alexanderbonnet <alex.bonnet.us@gmail.com>
2021-11-14 22:09:08 +0100

    minor formatting

--- python_binding/jacobi_method.py
+++ python_binding/jacobi_method.py
@@ -16,39 +16,41 @@ def main():
 
     ARGS = parser.parse_args()
 
-    out_path = Path("./src/bin")
-    if not out_path.exists():
-        out_path.mkdir(parents=True)
+    if not Path("./src/bin").exists():
+        Path("./src/bin").mkdir(parents=True)
 
     if ARGS.num_threads > 1:
-        compile_cmd = (
-            f"{CC} {CFLAGS} "
-            f"src/be_hpc_parallel.c {UTILS} "
-            f"-I {INCLUDE} "
-            f"-o src/bin/be_hpc_parallel.o"
-        )
-        subprocess.run(compile_cmd, shell=True)
+        compile_cmd = [
+            f"{CC} {CFLAGS}",
+            f"src/be_hpc_parallel.c {UTILS}",
+            f"-I {INCLUDE}",
+            "-o src/bin/be_hpc_parallel.o",
+        ]
+        subprocess.run(" ".join(compile_cmd), shell=True)
 
-        run_cmd = (
-            f"./src/bin/be_hpc_parallel.o "
-            f"{ARGS.problem_dimension} "
-            f"{ARGS.num_threads} "
-        )
-        subprocess.run(run_cmd, shell=True)
+        run_cmd = [
+            "./src/bin/be_hpc_parallel.o",
+            ARGS.problem_dimension,
+            ARGS.num_threads,
+        ]
+        subprocess.run(" ".join(str(item) for item in run_cmd), shell=True)
     elif ARGS.num_threads == 1:
-        compile_cmd = (
-            f"{CC} {CFLAGS} "
-            f"src/be_hpc_seq.c {UTILS} "
-            f"-I {INCLUDE} "
-            f"-o src/bin/be_hpc_seq.o"
-        )
-        subprocess.run(compile_cmd, shell=True)
+        compile_cmd = [
+            f"{CC} {CFLAGS}",
+            f"src/be_hpc_seq.c {UTILS}",
+            f"-I {INCLUDE}",
+            "-o src/bin/be_hpc_seq.o",
+        ]
+        subprocess.run(" ".join(compile_cmd), shell=True)
 
-        run_cmd = f"./src/bin/be_hpc_seq.o " f"{ARGS.problem_dimension} "
-        subprocess.run(run_cmd, shell=True)
+        run_cmd = [
+            "./src/bin/be_hpc_seq.o",
+            ARGS.problem_dimension,
+        ]
+        subprocess.run(" ".join(str(item) for item in run_cmd), shell=True)
     else:
         # explicitly handle error
-        raise ValueError("num_threads must be striclty larger than 1!")
+        raise ValueError("num_threads must be striclty larger than 1")
 
 
 if __name__ == "__main__":
